e03b8f04eb837b3051b999065e6b5201aa67482d
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2018-08-17 16:40:46 -0300

    add stories

--- m02/working-with-assets/github-app/src/components/actions/actions.story.js
+++ m02/working-with-assets/github-app/src/components/actions/actions.story.js
@@ -1,9 +1,19 @@
 'use strict'
 
-import { storiesOf } from '@kadira/storybook'
+import { storiesOf, action } from '@kadira/storybook'
 import React from 'react'
+import Actions from './index'
 
-storiesOf('Div', module)
-  .add('first story', () => (
-    <div>Hello</div>
+const stories = storiesOf('Div', module)
+stories.add('first story', () => (
+    <Actions
+      getRepos={action('Get Repos')}
+      getStarred={action('Get Starred')}
+    />
+  ))
+  stories.add('first story', () => (
+    <Actions
+      getRepos={action('Get Repos')}
+      getStarred={action('Get Starred')}
+    />
   ))
