30650004ae1780d4f1b13bb795963933e32ad13a
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2018-08-17 16:11:17 -0300

    add basic configure for storybook

--- m02/working-with-assets/github-app/.storybook/config.js
+++ m02/working-with-assets/github-app/.storybook/config.js
@@ -1,7 +1,10 @@
-import { configure } from '@kadira/storybook';
+import { configure } from '@kadira/storybook'
+
+const req = require.context('../src/components', true, /\.story\.js$/)
 
 function loadStories() {
-  require('../stories');
+  // require('../src/components/actions/actions.story');
+  req.keys().forEach( (filename) => req(filename) )
 }
 
 configure(loadStories, module);
