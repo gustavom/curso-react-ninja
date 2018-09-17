dba83ebbc45c8e7279497bf097292189e6a6f059
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2018-09-17 15:02:42 -0300

    fix path

--- m02/working-with-assets/github-app/storybook/webpack.config.js
+++ m02/working-with-assets/github-app/storybook/webpack.config.js
@@ -1,24 +1,37 @@
 'use strict'
 
-const webpackConfig = require('@kadira/storybook/dist')
+const webpackConfig = require('@kadira/storybook/dist/server/config/defaults/webpack.config.js')
 
-module.exports = {
-  module: {
-    preLoaders: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      include: /src/,
-      loader: 'standard'
-    }],
+// module.exports = {
+//   module: {
+//     preLoaders: [{
+//       test: /\.js$/,
+//       exclude: /node_modules/,
+//       include: /src/,
+//       loader: 'standard'
+//     }],
 
-    loaders:[{
-      test: /\.css$/,
-      exclude: /node_modules/,
-      include: /src/,
-      // loaders: ['style', 'css?modules&localIdentName=[local]']
-      loaders: ['style', 'css?modules&localIdentName=[path][name]__[local]--[hash:base64:15]']
-      // loaders: ['style','raw']
-    }]
+//     loaders:[{
+//       test: /\.css$/,
+//       exclude: /node_modules/,
+//       // include: /src/,
+//       // loaders: ['style', 'css?modules&localIdentName=[local]']
+//       loaders: ['style', 'css?modules&localIdentName=[path][name]__[local]--[hash:base64:15]']
+//       // loaders: ['style','raw']
+//     }]
 
-  }
+//   }
+// }
+
+
+module.exports = function (config. env) {
+  const newConfig = webpackConfig(config, env)
+
+  newConfig.mobule.preLoaders = (newConfig.module.preLoaders || []).concat({
+    test: /\.js$/,
+    exclude: /node_modules/,
+    include: /src/,
+    loader: 'standard'
+  })
+  return newConfig
 }
