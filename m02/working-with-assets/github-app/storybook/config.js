ed4e61849d0a0b52f3907bae892e081588ed4801
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2018-09-25 11:21:37 -0300

    update storybook configs

--- m02/working-with-assets/github-app/storybook/config.js
+++ m02/working-with-assets/github-app/storybook/config.js
@@ -2,9 +2,9 @@ import { configure } from '@kadira/storybook'
 
 const req = require.context('../src/components', true, /\.story\.js$/)
 
-function loadStories() {
+function loadStories () {
   // require('../src/components/actions/actions.story');
-  req.keys().forEach( (filename) => req(filename) )
+  req.keys().forEach((filename) => req(filename))
 }
 
 configure(loadStories, module)
--- m02/working-with-assets/github-app/storybook/webpack.config.js
+++ m02/working-with-assets/github-app/storybook/webpack.config.js
@@ -2,35 +2,13 @@
 
 const webpackConfig = require('@kadira/storybook/dist/server/config/defaults/webpack.config.js')
 
-// module.exports = {
-//   module: {
-//     preLoaders: [{
-//       test: /\.js$/,
-//       exclude: /node_modules/,
-//       include: /src/,
-//       loader: 'standard'
-//     }],
-
-//     loaders:[{
-//       test: /\.css$/,
-//       exclude: /node_modules/,
-//       // include: /src/,
-//       // loaders: ['style', 'css?modules&localIdentName=[local]']
-//       loaders: ['style', 'css?modules&localIdentName=[path][name]__[local]--[hash:base64:15]']
-//       // loaders: ['style','raw']
-//     }]
-
-//   }
-// }
-
-
 module.exports = function (config, env) {
   const newConfig = webpackConfig(config, env)
 
   newConfig.module.preLoaders = (newConfig.module.preLoaders || []).concat({
     test: /\.js$/,
     exclude: /node_modules/,
-    include: /src/,
+    // include: /src/,
     loader: 'standard'
   })
   return newConfig
